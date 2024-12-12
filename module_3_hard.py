955b7c78cf2795fdf761c44306d969a058dd6d7d
Timka <takhmeto2009@gmail.com>
2024-12-12 12:01:50 +0500

    redisigned_module_3_hard.py

--- module_3_hard.py
+++ module_3_hard.py
@@ -1,40 +1,25 @@
 def calculate_structure_sum(data_structure):
     s = 0
-    for i in data_structure:
-        if isinstance(i, list):
-            s += sum(i)
-        elif isinstance(i, tuple):
-            for h in i:
-                if isinstance(h, dict):
-                    s += sum(h.values())
-                    s += sum(len(key) for key in h.keys())
-                elif isinstance(h, tuple):
-                    s += sum(h)
-                elif isinstance(h, list):
-                    for g in h:
-                        for t in g:
-                            for p in t:
-                                if isinstance(p, str):
-                                    s += len(p)
-                                elif isinstance(p, tuple):
-                                    for k in p:
-                                        if isinstance(k, str):
-                                            s += len(k)
-                                        else:
-                                            s += k
-                                else:
-                                    s += p
-                else:
-                    s += h
-        elif isinstance(i, dict):
-            s += sum(i.values())
-            s += sum(len(key) for key in i.keys())
+    def recurse(i):
+        nonlocal s
+        if isinstance(i, (int, float)):
+            s += i
         elif isinstance(i, str):
             s += len(i)
+        elif isinstance(i, (tuple, list, set)):
+            for j in i:
+                recurse(j)
+        elif isinstance(i, dict):
+            for key, value in i.items():
+                recurse(key)
+                recurse(value)
         else:
-            s += i
-    return s
+            pass
 
+    for h in data_structure:
+        recurse(h)
+
+    return s
 
 data_structure = [[1, 2, 3], {'a': 4, 'b': 5}, (6, {'cube': 7, 'drum': 8}), "Hello", ((), [{(2, 'Urban', ('Urban2', 35))}])]
 
